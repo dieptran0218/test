ab4d822fd983f6e469737375167623175fe3917e
dieptran0218 <71701090+dieptran0218@users.noreply.github.com>
2020-11-26 16:24:27 +0700

    test

---
+++ hello/src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		dddd d = new dddd();
+                d.main(args);
+	}
+}
